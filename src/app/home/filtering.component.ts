15c02b2de71d8d8c98216304584ca750039f6c88
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-04 11:49:58 +0100

    Both sorting and filtering pages added

---
+++ src/app/home/filtering.component.ts
@@ -0,0 +1,4 @@
+import { Component } from '@angular/core';
+
+@Component({ templateUrl: 'filtering.component.html' })
+export class HomeComponent { }
---
+++ src/app/home/sorting.component.ts
@@ -0,0 +1,4 @@
+import { Component } from '@angular/core';
+
+@Component({ templateUrl: 'sorting.component.html' })
+export class HomeComponent { }
